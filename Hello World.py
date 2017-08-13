45d47eef59bf57791f857523ed87e4227a6feac3
Logan Lin <linyanwd213@gmail.com>
2017-08-13 14:42:40 +0800

    Initial commit

---
+++ Hello World.py
@@ -0,0 +1,66 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, SimpleRNN
+from keras.utils.np_utils import to_categorical
+import numpy as np
+from keras.utils import plot_model
+
+timesteps = 16
+data_dim = 1
+num_classes = 578
+
+batch = 64
+epoch = 20
+
+x_train = np.loadtxt('data_0812/x_train.txt', delimiter=',')
+x_val = np.loadtxt('data_0812/x_value.txt', delimiter=',')
+y_train = np.loadtxt('data_0812/y_train.txt', delimiter=',')
+y_val = np.loadtxt('data_0812/y_value.txt', delimiter=',')
+
+y_train = to_categorical(y_train, num_classes)
+y_val = to_categorical(y_val, num_classes)
+
+y_train.shape = -1, num_classes
+y_val.shape = -1, num_classes
+
+x_train.shape = -1, timesteps, data_dim
+x_val.shape = -1, timesteps, data_dim
+
+# expected input data shape: (batch_size, timesteps, data_dim)
+model = Sequential()
+model.add(LSTM(batch, return_sequences=True,
+               input_shape=(timesteps, data_dim)))
+model.add(LSTM(64, return_sequences=True))
+model.add(LSTM(64, return_sequences=True))
+model.add(LSTM(64, return_sequences=True))
+model.add(LSTM(64, return_sequences=True))
+model.add(LSTM(64))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+time = "0813_01"
+
+hist = model.fit(x_train, y_train,
+                 batch_size=batch, epochs=epoch)
+model.save_weights("save/data_" + time + ".h5")
+
+history_file = open("save/history_" + time + ".txt", "w")
+history_file.write("batch_size=" + str(batch) + "\n")
+history_file.write(str(hist.history))
+history_file.close()
+
+summary = str(model.to_json())
+summary_file = open("save/summary_file_" + time + ".txt", "w")
+summary_file.write("batch_size=" + str(batch) + "\n")
+summary_file.write(summary)
+summary_file.close()
+
+# model.load_weights(filepath="data.h5")
+
+plot_model(model, to_file='save/model_' + time + '.png', show_shapes=True)
+
+score = model.evaluate(x_val, y_val, batch_size=64)
+
+print("evaluate: %f" % (score[1]))
---
+++ draw.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def getInfo(filename):
+    loss_list = []
+    acc_list = []
+    count = 0
+    file = open(filename)
+    flag1 = True
+    flag2 = True
+    N = ''
+    while 1:
+        line = file.readline()
+        if not line:
+            break
+        # find Epoch
+        if line[0:5] == 'Epoch' and flag1:
+            index = line.find('/')
+            Epoch = line[index + 1:]
+            flag1 = False
+        # find number of x_train
+        if flag1 == False and flag2:
+            if line.find('/') > 0 and line.find('[') > 0:
+                index = line.find('/')
+                N = line[index + 1: index + 6]
+                flag2 = False
+
+        if line[0:5] == N:
+            index = line.find("loss:")
+            loss = line[index + 6: index + 12]
+            loss_list.append(loss)
+            index = line.find("acc:")
+            acc = line[index + 5: index + 11]
+            acc_list.append(acc)
+
+    return Epoch, loss_list, acc_list
+
+
+if __name__ == '__main__':
+    filename = "console.out"
+    Epoch, loss_list, acc_list = getInfo(filename)
+    index = np.arange(int(Epoch), dtype=np.int32)
+    plt.figure(1)
+    plt.plot(index, loss_list)
+    plt.savefig('loss.png')
+    plt.figure(2)
+    plt.plot(index, acc_list)
+    plt.savefig('acc.png')
+    plt.show()

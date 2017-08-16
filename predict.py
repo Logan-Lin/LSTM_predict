16003515340fd19f43e353a7c9d3531804a7ecb7
Logan Lin <juniormail@126.com>
2017-08-16 09:50:24 +0800

    Minor Adjustment

--- predict.py
+++ predict.py
@@ -8,6 +8,8 @@ import random
 timesteps = 16
 data_dim = 1
 num_classes = 628
+batch_size = 16
+epochs = 10
 
 train = np.loadtxt('data/train.txt', dtype=np.int32, delimiter=',')
 test = np.loadtxt('data/train.txt', dtype=np.int32, delimiter=',')
@@ -15,7 +17,7 @@ test = np.loadtxt('data/train.txt', dtype=np.int32, delimiter=',')
 train.shape = -1, 17
 test.shape = -1, 17
 
-random.shuffle(train)
+# random.shuffle(train)
 x_train = train[:, :-1]
 y_train = train[:, -1]
 y_train = to_categorical(y_train, num_classes)
@@ -29,10 +31,10 @@ y_test = test[:, -1]
 y_test1 = to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(LSTM(64, return_sequences=True,
+model.add(LSTM(batch_size, return_sequences=True,
                input_shape=(timesteps, data_dim)))
-model.add(LSTM(64, return_sequences=True))
-model.add(LSTM(64))
+model.add(LSTM(batch_size, return_sequences=True))
+model.add(LSTM(batch_size))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -46,15 +48,15 @@ model.compile(loss='categorical_crossentropy',
 model.summary()
 
 model.fit(x_train, y_train,
-          batch_size=64, epochs=1, shuffle=True)
+          batch_size=batch_size, epochs=epochs, shuffle=True)
 # validation_data=(x_val, y_val))
 
 model.save_weights("data5.h5")
 
 # plot_model(model, to_file='model.png', show_shapes=True)
-score = model.evaluate(x_test, y_test1, batch_size=64)
+score = model.evaluate(x_test, y_test1, batch_size=batch_size)
 
-pd = model.predict(x_test, batch_size=64)
+pd = model.predict(x_test, batch_size=batch_size)
 m, n = pd.shape
 Y = []
 for i in range(m):

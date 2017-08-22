a3b0d095c96fbfae21566f34294e8014bce33415
Logan Lin <juniormail@126.com>
2017-08-22 13:14:25 +0800

    Upload a new set of data

--- predict.py
+++ predict.py
@@ -4,33 +4,40 @@ from keras.layers import LSTM, Dense, Dropout
 from keras.utils.np_utils import to_categorical
 import numpy as np
 import random
+from keras import regularizers
+import keras
 
-timesteps = 16
+
+timesteps = 6
 data_dim = 1
-num_classes = 628
+num_classes = 26514
 batch_size = 64
 units = 64
-epochs = 10
-drop_rate = 0.2
+epochs = 500
+drop_rate = 0.1
+time = 1100
+batch = 64
 
-train = np.loadtxt('data/train.txt', dtype=np.int32, delimiter=',')
-test = np.loadtxt('data/test.txt', dtype=np.int32, delimiter=',')
+x_train = np.loadtxt('data/x_train.txt', delimiter=',')
+x_val = np.loadtxt('x_value.txt', delimiter=',')
+y_train = np.loadtxt('y_train.txt', delimiter=',')
+y_val = np.loadtxt('y_value.txt', delimiter=',')
 
-train.shape = -1, 17
-test.shape = -1, 17
+x_train.shape = -1,timesteps
+import random
+data = np.c_[x_train,y_train]
+random.shuffle(data)
+x_train = data[:, :-1]
+y_train = data[:, -1]
 
-# random.shuffle(train)
-x_train = train[:, :-1]
-y_train = train[:, -1]
+y = y_val
+y1 = y_train
 y_train = to_categorical(y_train, num_classes)
+y_val = to_categorical(y_val, num_classes)
 
-x_train.shape = -1, timesteps, data_dim
-y_train.shape = -1, num_classes
-
-x_test = test[:, :-1]
-x_test.shape = -1, timesteps, data_dim
-y_test = test[:, -1]
-y_test1 = to_categorical(y_test, num_classes)
+y_train.shape = -1,num_classes
+x_train.shape = -1,timesteps,data_dim
+x_val.shape = -1,timesteps,data_dim
 
 model = Sequential()
 model.add(LSTM(units, return_sequences=True,
@@ -41,8 +48,10 @@ model.add(LSTM(units, return_sequences=True))
 # model.add(LSTM(units, return_sequences=True))
 model.add(Dropout(drop_rate))
 model.add(LSTM(units))
-model.add(Dense(int(units * 4)))
 model.add(Dense(num_classes, activation='softmax'))
+# model.add(Dense(num_classes, activation='softmax', input_dim=units,
+#                 kernel_regularizer=regularizers.l2(0.01),
+#                 activity_regularizer=regularizers.l1(0.01)))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
@@ -51,26 +60,24 @@ model.compile(loss='categorical_crossentropy',
 # --------------------------------------------------------
 # model.load_weights(filepath="data5.h5")
 # --------------------------------------------------------
+early_stopping = keras.callbacks.EarlyStopping(monitor="val_acc", patience=50, mode="auto")
+
+hist = model.fit(x_train, y_train,
+                 validation_split=0.1,
+                 callbacks=[early_stopping],
+          batch_size=batch, epochs=epochs)
+
+model.save_weights("model/data" + str(time) + ".h5")
+
+history_file = open("history_" + str(time) + ".txt", "w")
+history_file.write("batch_size=" + str(batch) + "\n")
+history_file.write(str(hist.history))
+history_file.close()
 
 model.summary()
 
-model.fit(x_train, y_train,
-          batch_size=batch_size, epochs=epochs, shuffle=True)
-# validation_data=(x_val, y_val))
-
-model.save_weights("model/data.h5")
-
 # plot_model(model, to_file='model.png', show_shapes=True)
-score = model.evaluate(x_test, y_test1, batch_size=batch_size)
+score = model.evaluate(x_val, y_val, batch_size=batch_size)
 
-pd = model.predict(x_test, batch_size=batch_size)
-m, n = pd.shape
-Y = []
-for i in range(m):
-    Y.append(np.argmax(pd[i]))
-mm = 0
-for i in range(m):
-    if Y[i] == y_test[i]:
-        mm = mm + 1
-print("predict: %f" % (mm / m))
-print("evaluate: %f" % (score[1]))
+print("loss: %f \n" % (score[0]))
+print("evaluate: %f \n" % (score[1]))
